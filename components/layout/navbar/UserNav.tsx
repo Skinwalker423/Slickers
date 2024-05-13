d4ce90c4a06d79c9bdbb0b557ec2c3b3a080a7dd
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-05-12 19:04:23 -0700

    usernav

--- components/layout/navbar/UserNav.tsx
+++ components/layout/navbar/UserNav.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MenuIcon, User } from "lucide-react";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar";
 
 export const UserNav = () => {
-  return <div>UserNav</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className='flex items-center justify-center gap-x-3 rounded-full border px-2 py-2 lg:px-4 lg:py-2'>
+          <MenuIcon className='w-6 h-6 lg:w-5 lg:h5' />
+          <Avatar className='hidden md:block'>
+            <AvatarImage src='https://github.com/shadcn.png' />
+            <AvatarFallback>
+              <User />
+            </AvatarFallback>
+          </Avatar>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Register</DropdownMenuItem>
+        <DropdownMenuItem>Login</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
